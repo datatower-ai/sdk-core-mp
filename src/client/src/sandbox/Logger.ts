2cce7e39ce5cfe634a70e13c3646fc48fad3277c
shewulong <shewulong@lovinjoy.com>
2024-04-24 14:41:38 +0800

    feat(client): logger

--- src/client/src/sandbox/Logger.ts
+++ src/client/src/sandbox/Logger.ts
@@ -7,26 +7,26 @@ export class Logger {
   static level: LogLevel = LogLevel.VERBOSE;
   static verbose(...args: any[]) {
     if (this.level > LogLevel.VERBOSE) return;
-    console.log('[VERBOSE]', ...args);
+    console.log('%c[VERBOSE]', 'color: gray', ...args);
   }
   static assert(condition?: boolean | undefined, ...args: any[]) {
     if (this.level > LogLevel.ASSERT) return;
-    console.assert(condition, '[ASSERT]', ...args);
+    console.assert(condition, '%c[ASSERT]', 'color: purple', ...args);
   }
   static debug(...args: any[]) {
     if (this.level > LogLevel.DEBUG) return;
-    console.debug('[DEBUG]', ...args);
+    console.debug('%c[DEBUG]', 'color: blue', ...args);
   }
   static info(...args: any[]) {
     if (this.level > LogLevel.INFO) return;
-    console.info('[INFO]', ...args);
+    console.info('%c[INFO]', 'color: green', ...args);
   }
   static warn(...args: any[]) {
     if (this.level > LogLevel.WARN) return;
-    console.warn('[WARN]', ...args);
+    console.warn('%c[WARN]', 'color: orange', ...args);
   }
   static error(...args: any[]) {
     if (this.level > LogLevel.ERROR) return;
-    console.error('[ERROR]', ...args);
+    console.error('%c[ERROR]', 'color: red', ...args);
   }
 }
