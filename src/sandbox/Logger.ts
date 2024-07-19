6a01e5328a54b5e65da4df024938228eafd78e91
shewulong <shewulong@lovinjoy.com>
2024-07-19 12:23:01 +0800

    feat: log添加DataTower标志信息

--- src/sandbox/Logger.ts
+++ src/sandbox/Logger.ts
@@ -7,26 +7,26 @@ export class Logger {
   static level: LogLevel = LogLevel.VERBOSE;
   static verbose(...args: any[]) {
     if (this.level > LogLevel.VERBOSE) return;
-    console.log('%c[VERBOSE]', 'color: gray', ...args);
+    console.log('%c[DataTower:VERBOSE]', 'color: gray', ...args);
   }
   static assert(condition?: boolean | undefined, ...args: any[]) {
     if (this.level > LogLevel.ASSERT) return;
-    console.assert(condition, '%c[ASSERT]', 'color: purple', ...args);
+    console.assert(condition, '%c[DataTower:ASSERT]', 'color: purple', ...args);
   }
   static debug(...args: any[]) {
     if (this.level > LogLevel.DEBUG) return;
-    console.debug('%c[DEBUG]', 'color: blue', ...args);
+    console.debug('%c[DataTower:DEBUG]', 'color: blue', ...args);
   }
   static info(...args: any[]) {
     if (this.level > LogLevel.INFO) return;
-    console.info('%c[INFO]', 'color: green', ...args);
+    console.info('%c[DataTower:INFO]', 'color: green', ...args);
   }
   static warn(...args: any[]) {
     if (this.level > LogLevel.WARN) return;
-    console.warn('%c[WARN]', 'color: orange', ...args);
+    console.warn('%c[DataTower:WARN]', 'color: orange', ...args);
   }
   static error(...args: any[]) {
     if (this.level > LogLevel.ERROR) return;
-    console.error('%c[ERROR]', 'color: red', ...args);
+    console.error('%c[DataTower:ERROR]', 'color: red', ...args);
   }
 }
