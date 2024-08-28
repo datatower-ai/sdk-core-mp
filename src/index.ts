88ffa514450a1f3352dda54e79eec254bf3b6b88
shewulong <shewulong@lovinjoy.com>
2024-08-28 12:22:48 +0800

    build: 发布为三方包

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { DataTower as Cocos } from '@/cocos';
+export { CocosAndroid } from '@/cocos/Android';
+export { CocosIOS } from '@/cocos/IOS';
+export { LogLevel } from '@/Logger';
+export * from '@/sandbox/exports';
+export type * from '@/type';
